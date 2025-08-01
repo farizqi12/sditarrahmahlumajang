e3e7730b0236a5791febc05714bc47fc7ceeb2a2
SovikiGo <sovikilmj@gmail.com>
2025-08-01 15:46:49 +0700

    perbaikan ui login ciak

--- resources/views/admin/courses.blade.php
+++ resources/views/admin/courses.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    
     <title>Manajemen Kelas</title>
     <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- For AJAX -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
